aa261676f2cfc24495666ea08ace139f2d8152eb
Atanas Atanasov <djx700@abv.bg>
2023-03-20 15:49:46 +0200

    yield return instead of Enumeration logic class

---
+++ CSharp-Advanced/08.Iterators And Comparators/0.Demo/Loop-A-Class-Advanced-yield return/Student.cs
@@ -0,0 +1,41 @@
+﻿using Loop_A_Class_Advanced_Enumerator;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_A_Class_GetEnumerator
+{
+    public class Student : IEnumerable<string> // implement IEnumerable<T> and below 2 methods appeared -> first is the most important.
+    {
+        private readonly List<string> studentProperties;
+        public Student(string id, string name, string currentGrade)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.CurrentGrade = currentGrade;
+            Grades = new List<double>() {2.5,3.6,5.6,5.9,3.1 };
+            studentProperties = new List<string>() { this.Id, this.Name, this.CurrentGrade , String.Join("\r\n", this.Grades)};
+        }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string CurrentGrade { get; set; }
+        public List<double> Grades { get; set; }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            // When i want simple implementation i can achieve it by yield return ( no need separate Enumeration Logic class)
+            for (int i = 0; i < studentProperties.Count(); i++)
+            {
+                yield return studentProperties[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
