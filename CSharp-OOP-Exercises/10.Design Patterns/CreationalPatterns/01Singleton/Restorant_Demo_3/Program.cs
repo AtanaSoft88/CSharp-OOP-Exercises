f6cf314ef18e75bf747c129c0f336e3a8d6e6d33
Atanas Atanasov <djx700@abv.bg>
2023-03-26 00:41:33 +0200

    singleto dp add

---
+++ CSharp-OOP-Exercises/10.Design Patterns/CreationalPatterns/01Singleton/Restorant_Demo_3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Restorant
+{
+    internal class Program
+    {
+        static Waiter tableWaiter1 = Waiter.GetTablesHandled();
+        static Waiter tableWaiter2 = Waiter.GetTablesHandled();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                ClientOneGetNextWaiter();
+                ClientTwoGetNextWaiter();
+            }
+        }
+        private static void ClientOneGetNextWaiter() 
+        {
+            Console.WriteLine($"The next waiter is: {tableWaiter1.GetNextWaiter()}");
+        }
+
+        private static void ClientTwoGetNextWaiter()
+        {
+            Console.WriteLine($"The next waiter is: {tableWaiter2.GetNextWaiter()}");
+        }
+    }
+}
