df41d76b8e616c43f9675bfc1380775a68d1ea68
Atanas Atanasov <djx700@abv.bg>
2023-03-25 18:00:08 +0200

    Test commit

---
+++ CSharp-OOP-Exercises/10.Design Patterns/CreationalPatterns/01Singleton/Singleton_Demo_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //At this point, we can call the Instance property as many times as we want,
+            //but our object is going to be instantiated only once and shared for every other call. 
+            var data = SingletonData.Instance;
+            var data1 = SingletonData.Instance;
+            var data2 = SingletonData.Instance;
+            var data3 = SingletonData.Instance;
+            string countryName = "Bulgaria";
+            Console.WriteLine($"Country: {countryName} population is :> {data.GetPopulation(countryName)} <:");
+           
+        }
+    }
+}
---
+++ CSharp-OOP-Exercises/10.Design Patterns/CreationalPatterns/01Singleton/Singleton_Demo_1/SingletonData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Demo
+{
+    public class SingletonData : ISingletonContainer
+    {
+        private static SingletonData instance = new SingletonData();
+        private Dictionary<string, int> countries = new Dictionary<string, int>();
+        private SingletonData()
+        {
+            Console.WriteLine("Initializing Singleton object");
+            var elements = File.ReadAllText("capitals.txt").Split(", ");
+            
+            for (int i = 0; i < elements.Length; i++)
+            {
+                countries.Add(elements[i].Split()[0], int.Parse(elements[i].Split()[1]));
+            }
+        }
+        public static SingletonData Instance => instance;
+        public int GetPopulation(string name)
+        {
+            return countries[name];
+        }
+    }
+}
